78a466bc9066adf473c70a11b2e1ab11913b33f9
Sabrina Hill <125325753+brinalynn17@users.noreply.github.com>
2023-10-30 16:24:26 +0000

    profile changes

--- app.js
+++ app.js
@@ -1,32 +1,94 @@
-// Get the input elements by their IDs
-const includeCoverInput = document.getElementById("includeCover")
-const fNameInput = document.getElementById("fName");
-const lNameInput = document.getElementById("lName");
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to get the value of an input element or return an empty string
+    function getInputValue(input) {
+        return input ? input.value : '';
+    }
+    // Handle social media links
+    const socialLinks = {
+        facebook: 'Facebook',
+        instagram: 'Instagram',
+        linkedin: 'LinkedIn',
+        twitter: 'Twitter',
+        github: 'GitHub'
+    };
 
-const includeCoverSpan = document.getElementById("Include_Cover")
-const firstNameSpan = document.getElementById("First_Name");
-const lastNameSpan = document.getElementById("Last_Name");
-
-function bindText(input, textElement) {
-    input.addEventListener("input", ()=>{
-        textElement.innerText = input.value
-    })
+    // Add event listeners to each social media icon
+    for (const name in socialLinks) {
+        const platform = socialLinks[name];
+        const inputElement = document.getElementById(platform);
+        const iconElement = document.querySelector(`.fa-${platform.toLowerCase()}`);
+        if (inputElement && iconElement) {
+            iconElement.parentElement.addEventListener('click', function (e) {
+                e.preventDefault();
+                let link = inputElement.value;
+                if (link) {
+                    if(!link.startsWith('http://') && !link.startsWith('https://')){
+                        link = 'https://' + link;
+                    }
+                    window.open(link, '_blank');
+                }
+        });
+    }
 }
 
-bindText(includeCoverInput, includeCoverSpan)
-bindText(fNameInput, firstNameSpan)
-bindText(lNameInput, lastNameSpan)
+//Variables
+    const includeCoverInput = document.getElementById("includeCover");
+    const coverImage = document.querySelector('.image-top'); // Add cover image
+    const fNameInput = document.getElementById("fName");
+    const lNameInput = document.getElementById("lName");
+    const fullNameElement = document.querySelector('.fullname');
+    const positionIconsInput = document.getElementById("positionIcons");
+    const socialIconsList = document.querySelector('.social-icons');
+    const roleInput = document.getElementById("role");
+    const roleElement = document.querySelector('.job');
+    const cityInput = document.getElementById("city");
+    const countryInput = document.getElementById("country");
+    const locationElement = document.querySelector('.location');
+    
+    // Update cover image based on the 'includeCover' selection
+    includeCoverInput.addEventListener("change", function() {
+        const selectedValue = includeCoverInput.value;
+      
+        if (selectedValue === "true") {
+          // If "Yes" is selected, set the image source to the desired image
+          coverImage.src = "img/Cash4Life.jpg"; // Update the image source
+          coverImage.style.display = "block"; // Display the image
+        } else if (selectedValue === "false") {
+          // If "No" is selected, hide the image
+          coverImage.style.display = "none"; // Hide the image
+        }
+    }); 
+    // Update profile card with First Name and Last Name 
+    fNameInput.addEventListener("input", function() {
+        fullNameElement.textContent = `${fNameInput.value} ${lNameInput.value}`; 
+    }); 
+    lNameInput.addEventListener("input", function() { 
+        fullNameElement.textContent = `${fNameInput.value} ${lNameInput.value}`;
+    }); 
+    // Handle the position change for social icons
+    positionIconsInput.addEventListener("change", function () {
+        const selectedPosition = positionIconsInput.value;
+        socialIconsList.classList.toggle('move-icons-left', selectedPosition === "left");
+    });
+    // Handle the role selection
+    roleInput.addEventListener("change", function () {
+        const selectedRole = roleInput.value;
+        roleElement.textContent = selectedRole;
+    });
+    // Handle the City selection
+    cityInput.addEventListener("change", function () {
+        const selectedCity = cityInput.value;
+        updateLocation(selectedCity, countryInput.value);
+    });
 
-// Add an event listener to listen for changes in the input fields
-// fNameInput.addEventListener("input", updateFirstName);
-// lNameInput.addEventListener("input", updateLastName);
+    // Handle the Country selection
+    countryInput.addEventListener("change", function () {
+        const selectedCountry = countryInput.value;
+        updateLocation(cityInput.value, selectedCountry);
+    });
+    
+    function updateLocation(city, country) {
+        locationElement.textContent = `${city}, ${country}`;
+    }
 
-// // Function to update the first name in the span
-// function updateFirstName() {
-//     firstNameSpan.textContent = fNameInput.value;
-// }
-
-// // Function to update the last name in the span
-// function updateLastName() {
-//     lastNameSpan.textContent = lNameInput.value;
-// }
+});
